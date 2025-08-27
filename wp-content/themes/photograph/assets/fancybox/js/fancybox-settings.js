c90f6d5351e2e331fdcb14d1766f858ac6b4453a
learningdotnet17 <learningdotnet17>
2025-08-27 10:43:36 +0530

    Cursor fixes

--- wp-content/themes/photograph/assets/fancybox/js/fancybox-settings.js
+++ wp-content/themes/photograph/assets/fancybox/js/fancybox-settings.js
@@ -1,5 +1,6 @@
 jQuery( function() {
-	jQuery('[data-fancybox="gallery, post-gallery"]').fancybox({
+	// Configure FancyBox for standard gallery images
+	jQuery('[data-fancybox="gallery"], [data-fancybox="post-gallery"]').fancybox({
 		animationDuration: 700,
 		transitionEffect: 'slide',
 		transitionDuration: 900,
@@ -11,5 +12,33 @@ jQuery( function() {
 	        "thumbs",
 	        "close"
 	    ]
+	});
+	
+	// Configure FancyBox for all photo galleries
+	jQuery('[data-fancybox="lighthouse-gallery"], [data-fancybox="flowers-gallery"], [data-fancybox="national-parks-gallery"], [data-fancybox="moon-gallery"], [data-fancybox="city-gallery"], [data-fancybox="sunsets-gallery"], [data-fancybox="prints-gallery"], [data-fancybox="night-gallery"], [data-fancybox="long-exposure-gallery"]').fancybox({
+		animationDuration: 700,
+		transitionEffect: 'slide',
+		transitionDuration: 900,
+		idleTime: 3,
+		buttons: [
+	        "zoom",
+	        'slideShow',
+	        'fullScreen',
+	        "thumbs",
+	        "close"
+	    ],
+		// Group all images in the same gallery together
+		loop: true,
+		thumbs: {
+			autoStart: true
+		},
+		// Show captions and image info
+		caption: function(instance, item) {
+			return item.opts.caption || '';
+		},
+		// Image counter
+		infobar: true,
+		// Enable keyboard navigation
+		keyboard: true
 	});
 });
